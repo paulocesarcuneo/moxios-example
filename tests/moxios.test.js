43352a16eb486b6dc4668d3816066b97d382b64e
Paulo Cesar Cuneo <cuneo.paulo.cesar@gmail.com>
2022-10-04 16:09:07 -0300

    chore:  comments

--- tests/moxios.test.js
+++ tests/moxios.test.js
@@ -12,6 +12,9 @@ describe("GET /user", function () {
   });
 
   it("expect mocked response", async () => {
+    // Usamos un regex para no tener que escribir la url exacta 
+    // le match como https://reqres.in/api/users/2 termina en /users/2
+    // con la regex se puede matchear el final del string
     moxios.stubRequest(/.*\/users\/2/, {
       status: 200,
       response: { email: "hello@mail.com" },
@@ -26,11 +29,15 @@ describe("GET /user", function () {
       status: 200,
       response: { email: "hello@mail.com" },
     });
+    // como /users/3 no esta configurado es necesario agregar 
+    // un match default de otra forma va colgar el test
     moxios.stubRequest(/.*/, {
       status: 500,
       response: { message: "error" },
     });
     const response = await request.get("/users/3");
+    // deberia matcher el default y por tanto propagar el 500
+    // segun la logica en routes
     expect(response.status).toBe(500);
   });
 
@@ -41,6 +48,9 @@ describe("GET /user", function () {
     });
     const response = await request.get("/users/2");
     expect(response.status).toBe(200);
+    // moxios.requests es la lista de request que recibio moxios
+    // mostRecent() devuelve el ultimo.
+    // ademas de url se puede inspecionar el body y los headers.
     expect(moxios.requests.mostRecent().url).toBe(
       "https://reqres.in/api/users/2"
     );
